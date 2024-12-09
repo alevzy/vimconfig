e5195c4700cffa5155928f0bfe9a363061d97617
alevzy <alevzy@github.com>
2024-12-10 01:05:57 +0300

    Fix bugs in clone_all.py

--- clone_all.py
+++ clone_all.py
@@ -11,7 +11,7 @@ class Plugin:
         self.__url = url
         self.__vimdir = vimdir
         self.__shell_commands = list()
-        self.__git_options = ''
+        self.__git_options = [] 
 
     @property
     def name(self):
@@ -25,7 +25,7 @@ class Plugin:
     @property
     def git_options(self):
         return self.__git_options
-    @git_options.setter:
+    @git_options.setter
     def git_options(self, value):
         self.__git_options = value
     def add_shell_command(self, command):
@@ -40,15 +40,15 @@ plugins = dict()
 plugins['fugitive'] = Plugin('fugitive', 'https://tpope.io/vim/fugitive.git', 'tpope/start/fugitive')
 plugins['fugitive'].add_helptags('fugitive/doc')
 plugins['NERDTree'] = Plugin('NERDTree', 'https://github.com/preservim/nerdtree.git', 'vendor/start/nerdtree')
-plugins['NERDTree'].add_helptags(os.path.join(VIM_PACK_DIR, 'vendor/start/nerdtree/doc')
+plugins['NERDTree'].add_helptags(os.path.join(VIM_PACK_DIR, 'vendor/start/nerdtree/doc'))
 plugins['vim-airline'] = Plugin('vim-airline', 'https://github.com/vim-airline/vim-airline', 'dist/start/vim-airline')
-plugins['vim-airline'].add_helptags(os.path.join(VIM_PACK_DIR, 'dist/start/vim-airline/doc')
+plugins['vim-airline'].add_helptags(os.path.join(VIM_PACK_DIR, 'dist/start/vim-airline/doc'))
 plugins['vim-airline-themes'] = Plugin('vim-airline-themes', 'https://github.com/vim-airline/vim-airline-themes', 'dist/start/vim-airline-themes')
-plugins['vim-airline-themes'].add_helptags(os.path.join(VIM_PACK_DIR, 'dist/start/vim-airline-themes/doc')
+plugins['vim-airline-themes'].add_helptags(os.path.join(VIM_PACK_DIR, 'dist/start/vim-airline-themes/doc'))
 plugins['youcompleteme'] = Plugin('youcompleteme', 'https://github.com/ycm-core/YouCompleteMe', 'YouCompleteMe/opt/YouCompleteMe')
 plugins['youcompleteme'].add_shell_command('pushd ' + os.path.join(VIM_PACK_DIR, plugins['youcompleteme'].vimdir)) 
 plugins['youcompleteme'].add_shell_command('python install.py --clangd-completer')
-pluguns['youcompleteme'].add_shell_command('popd')
+plugins['youcompleteme'].add_shell_command('popd')
 plugins['youcompleteme'].git_options = ['--recursive-submodules']
 plugins['gruvbox'] = Plugin('gruvbox', 'https://github.com/morhetz/gruvbox.git', 'default/start/gruvbox')
 
@@ -59,6 +59,7 @@ def clone_all(clone_dir):
     for plugin in plugins.values():
         print(f'Downloading {plugin.name}...')
         path = os.path.join(clone_dir, plugin.name)
+        shutil.rmtree(path)
         subprocess.run(['git', 'clone'] + plugin.git_options + [plugin.url, path])
         print('Done')
 
@@ -70,7 +71,10 @@ def install(clone_dir):
             source_path = plugin.url
         else:
             source_path = os.path.join(clone_dir, plugin.name)
+            shutil.rmtree(source_path)
         subprocess.run(['git', 'clone'] + plugin.git_options + [source_path, install_path])
+        for command in plugin.iterate_commands():
+            subprocess.run(command.split(' '))
         print(f'Done')    
     shutil.copyfile(os.path.abspath('./.vimrc'), '~/.vimrc')
 
@@ -79,10 +83,10 @@ def main():
     parser.add_argument('--clonedir', type=str)
     parser.add_argument('--download', action='store_true')
     parser.add_argument('--install', action='store_true')
-    parser.parse_args()
-    clone_dir = parser.clone_dir
-    download_flag = parser.download
-    install_flag = parser.install
+    args = parser.parse_args()
+    clone_dir = args.clonedir
+    download_flag = args.download
+    install_flag = args.install
     
     if clone_dir is not None and download_flag:
         clone_all(clone_dir)
